00c7059054613cd82d484e5918a760ccc7ff734c
Andrey Sidorov <takandar@gmail.com>
2017-09-26 14:05:55 +0300

    do not create database.yml here

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,31 +5,8 @@
 # Copyright 2017, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
-
-app = AppHelpers.new node['app']
 
 # for pg gem
 package 'libpq-dev'
 
-# for hstore extension
-# include_recipe 'postgresql::contrib'
 include_recipe 'postgresql::server'
-
-template "#{app.dir(:shared)}/config/database.yml" do
-  source 'database.yml.erb'
-
-  database = "#{app.name}_#{app.env}"
-  user = node['postgresql']['users'].find { |v| v['username'] == database }
-
-  variables(
-    app_env: app.env,
-    database: database,
-    username: user['username'],
-    password: user['password']
-  )
-
-  owner app.user
-  group app.group
-  mode '0660'
-end
